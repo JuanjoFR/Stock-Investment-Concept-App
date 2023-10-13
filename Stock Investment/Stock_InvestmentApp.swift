7fe63c5c9463124fcd59ba0c74138dbb46b478cf
Juanjo Fernández <juanjo.fernandez@haa.st>
2023-10-13 10:30:51 +0200

    Initial Commit

---
+++ Stock Investment/Stock_InvestmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stock_InvestmentApp.swift
+//  Stock Investment
+//
+//  Created by Juanjo Fernández Romeo on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Stock_InvestmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
